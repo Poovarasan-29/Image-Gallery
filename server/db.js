4cd162c2dd610a77e6cd48dd761f59a982c66f7d
unknown <spking222005@gmail.com>
2024-12-26 15:08:17 +0530

    DB connected

--- server/db.js
+++ server/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-
+const MONGODB_URL = "mongodb+srv://spking222005:SayHi_29@imagegallery.bjehx.mongodb.net/gallery"
 const connectDB = async () => {
     try {
-        const con = await mongoose.connect('mongodb://127.0.0.1:27017/gallery');
+        const con = await mongoose.connect(MONGODB_URL);
+        
         console.log("Database Connected : ", con.connection.host);
     } catch (error) {
         console.log("Database Not Connected");
     }
-}
+} 
 
 module.exports = connectDB;
